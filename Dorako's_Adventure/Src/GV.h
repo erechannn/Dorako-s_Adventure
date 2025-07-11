97dcc892f1b2162af5122c142e181a6e85134e9a
yamadamasaki <g023g1002@g.neec.ac.jp>
2025-07-11 16:24:48 +0900

    プリコンパイルヘッダー追加・グローバル変数を扱うファイル追加

---
+++ Dorako's_Adventure/Src/GV.h
@@ -0,0 +1,31 @@
+﻿#ifndef GV_H_
+#define GV_H_
+
+// デバッグビルドかをチェック
+#ifdef _DEBUG
+namespace GVDetail {
+    constexpr bool is_debug = true;
+}
+#else
+namespace GVDetail {
+    constexpr bool is_debug = false;
+}
+#endif
+
+
+// グローバル変数にアクセス
+#define GLOBAL GV::instance()
+
+// グローバル変数を管理するシングルトン構造体
+// GlobalVariableの略
+struct GV {
+    // インスタンスを取得
+    static GV& instance() {
+        static GV self;
+        return self;
+    }
+
+    /* グローバル関数*/
+};
+
+#endif // !GV_H_
