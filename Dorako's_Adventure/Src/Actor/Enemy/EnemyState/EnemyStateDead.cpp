570d2f25f2fce5ba70adcf5df164b1ddfba0185e
yamadamasaki <g023g1002@g.neec.ac.jp>
2026-02-24 22:41:46 +0900

    KuriboにFireItemをスポーンさせる

--- Dorako's_Adventure/Src/Actor/Enemy/EnemyState/EnemyStateDead.cpp
+++ Dorako's_Adventure/Src/Actor/Enemy/EnemyState/EnemyStateDead.cpp
@@ -10,6 +10,9 @@ void EnemyStateDead::update(float delta_time) {
 	owner_->dead(delta_time);
 	owner_->mesh_->change_motion(EnemiesMotion::Dead, false);
 	if (owner_->mesh_->is_end_motion()) {
+		if (owner_->name() == "Kuribo") {
+			owner_->spawn_item(Item::Fire);
+		}
 		if (owner_->tag() == "BossEnemyTag") {
 			owner_->spawn_item(Item::CoinItem);
 		}
